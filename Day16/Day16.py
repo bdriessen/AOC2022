e8c5b832589cbac26dc83ca37850fe05e70883f2
Bart Driessen <bart.driessen@gmail.com>
2023-01-11 12:29:35 +0100

    Reduced graph size by eliminating zero flow rates.

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -25,8 +25,8 @@ def read_input_file(fn):
         tokens = list(filter(None, tokens))
         node = {'name': tokens[1], 'rate': int(tokens[5]), 'to': tokens[10:]}
         nodes.append(node)
-    for node in nodes:
-        print(node)
+#    for node in nodes:
+#        print(node)
 
     return nodes
 
@@ -35,9 +35,59 @@ def parse_input():
     return 0
 
 
+def clean_graph(graph):
+    # remove all nodes with 2 negihbors and a rate of 0
+    newgraph = graph.copy()
+    print("Now checking: ", list(graph.nodes(data=True)))
+    nodes_to_remove = []
+    for node in graph.nodes():
+        print("Investigating node: ", node, "with rate: ", newgraph.nodes[node]['rate'])
+        if graph.nodes[node]["rate"] == 0 and len(list(graph.neighbors(node))) == 2:
+            nodes_to_remove.append(node)
+            print("Found node to remove: ", node)
+#            neighbors = list(graph.neighbors(node))
+#            newgraph.add_edge(neighbors[0], neighbors[1], weight=1)
+
+    print("Nodes to remove: ", nodes_to_remove)
+    for node in nodes_to_remove:
+        neighbors = list(newgraph.neighbors(node))
+        newweight = newgraph.get_edge_data(neighbors[0], node)['weight'] + newgraph.get_edge_data(neighbors[1], node)['weight']
+        newgraph.add_edge(neighbors[0], neighbors[1], weight=newweight)
+        newgraph.remove_node(node)
+    return newgraph
+
+
+
 # Part 1
 def part1(fn):
     nodes = read_input_file(fn)
+    G = nx.Graph()
+
+    for node in nodes:
+        G.add_node(node['name'], rate=node['rate'])
+        for to in node['to']:
+            G.add_edge(node['name'], to, weight=1)
+
+    cleanG = clean_graph(G)
+
+    pos = nx.spring_layout(cleanG)
+    pos2 = nx.spring_layout(G)
+
+
+    nx.draw(cleanG, pos, with_labels=True)
+    edge_labels = nx.get_edge_attributes(cleanG, 'weight')
+    nx.draw_networkx_edge_labels(cleanG, pos, edge_labels=edge_labels)
+
+    # for k, v in pos2.items():
+    #     # Shift the x values of every node by 10 to the right
+    #     v[0] = v[0] + 10
+    # nx.draw(G, pos2, with_labels=True)
+    # edge_labels = nx.get_edge_attributes(G, 'weight')
+    # nx.draw_networkx_edge_labels(G, pos2, edge_labels=edge_labels)
+
+
+    plt.show()
+
     return 0
 
 
