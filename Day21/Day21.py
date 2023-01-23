3e9e980f64790d764831011fe4277dddcb179dba
Bart Driessen <bart.driessen@gmail.com>
2023-01-23 22:06:48 +0100

    Day 21 part 1 ok.

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -11,7 +11,13 @@
 
 # Read input file
 def read_input_file(fn):
-    return
+    with open(fn) as f:
+        lines = f.readlines()
+        # remove \n and empty strings
+        lines = [x.strip() for x in lines if x.strip()]
+        # replace colon with equal sing
+        lines = [x.replace(":", "=") for x in lines]
+    return lines
 
 def parse_input():
     return
@@ -19,26 +25,52 @@ def parse_input():
 
 # Part 1
 def part1(fn):
-    return 0
+    lines = read_input_file(fn)
+    solved = False
+    while not solved:
+        for line in lines:
+            try:
+                exec(line)
+            except NameError:
+                pass
+            if 'root' in locals():
+                solved = True
+                break
+    solution = int(eval('root'))
+
+    return solution
 
 # Part 2
 def part2(fn):
+    lines = read_input_file(fn)
+    # find line starting with "root"
+    for idx, line in enumerate(lines):
+        if line.startswith("humn"):
+            idx_humn = idx
+            break
+    # Replace
+
     return 0
 
 
 def main():
-    test = True
+    real = True
     part = 1
 
-    if test:
-        fn = "Day21/input.txt"
-    else:
-        fn = "Day21/input_test.txt"
+
 
     if part == 1:
+        if real:
+            fn = "Day21/input.txt"
+        else:
+            fn = "Day21/input_test.txt"
         res1 = part1(fn)
         print("Part 1: ", res1)
     else:
+        if real:
+            fn = "Day21/input2.txt"
+        else:
+            fn = "Day21/input_test2.txt"
         res2 = part2(fn)
         print("Part 2: ", res2)
     return
