a349f98e9ebceabf89830b7749a86d61c74aef3c
Bart Driessen <bart.driessen@gmail.com>
2023-01-26 17:03:26 +0100

    Day 20: not yet

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -7,6 +7,7 @@
 # Part 2 done:
 #
 
+import re
 
 
 # Read input file
@@ -44,42 +45,67 @@ def part1(fn):
 def part2(fn):
     lines = read_input_file(fn)
     # find line starting with "root"
-    idx_humn = 0
-    for idx, line in enumerate(lines):
-        if line.startswith("humn"):
-            idx_humn = idx
+    for line in lines:
+        if line.startswith("root"):
+            root = line
             break
 
-    solved = False
-    human = 0
-    while not solved:
-        human_str = "humn = " + str(human)
-        print(human_str)
-        name_error_exist = True
-        while name_error_exist:
-            name_error_exist = False
-            for line in lines:
-                try:
-                    if line.startswith("humn"):
-                        exec(human_str)
-                    else:
-                        exec(line)
-                except NameError:
-                    name_error_exist = True
-            if 'root' in locals():
-                print("human: ", human, "root: ", eval('root'))
-                if eval('root') == True:
-                    solved = True
-                    break
-                else:
-                    human += 1
-                    break
-    solution = human
-    print(eval('pppw'))
-    print(eval('sjmn'))
+    tokens = []
+    for line in lines:
+        # Split line into words using ":" or " " as separator
+        words = re.split(r'[: ]', line)
+        # remove '=' from first word in words
+        words[0] = words[0].replace("=", "")
+        tokens.append(words)
 
+    solved = False
+    while not solved:
+        for formula in tokens:
+            # if len (formula) == 2, then it is a value
+            if len(formula) == 2 and formula[0] != "humn":
+                exec(formula[0] + " = " + formula[1])
+                # print(formula[0] + " = " + formula[1])
+            elif len(formula) == 4:
+                res = formula[0]
+                op1 = formula[1]
+                op = formula[2]
+                op2 = formula[3]
+                if res == 'root':
+                    if op1 in locals():
+                        exec(op2 + " = " + op1)
+                    elif op2 in locals():
+                        exec(op1 + " = " + op2)
+                elif res in locals():
+                    if op1 in locals() and op2 not in locals():
+                        if op2 == '+':
+                            exec(op2 + " = " + res + "-" + op1)
+                        elif op2 == '*':
+                            exec(op2 + " = " + res + "//" + op1)
+                        elif op2 == '/':
+                            exec(op2 + " = " + res + "*" + op1)
+                        elif op2 == '-':
+                            exec(op2 + " = " + res + "+" + op1)
+                    elif op1 not in locals() and op2 in locals():
+                        if op1 == '+':
+                            exec(op1 + " = " + res + "-" + op2)
+                        elif op1 == '*':
+                            exec(op1 + " = " + res + "//" + op2)
+                        elif op1 == '/':
+                            exec(op1 + " = " + res + "*" + op2)
+                        elif op1 == '-':
+                            exec(op1 + " = " + res + "+" + op2)
+                else:
+                    if op1 in locals() and op2 in locals():
+                        exec(res + " = " + op1 + op + op2)
+
+        if 'humn' in locals():
+            solved = True
+            break
+    solution = int(eval('humn'))
 
     return solution
+
+
 
 
 def main():
@@ -97,9 +123,9 @@ def main():
         print("Part 1: ", res1)
     else:
         if real:
-            fn = "Day21/input2.txt"
+            fn = "Day21/input.txt"
         else:
-            fn = "Day21/input_test2.txt"
+            fn = "Day21/input_test.txt"
         res2 = part2(fn)
         print("Part 2: ", res2)
     return
